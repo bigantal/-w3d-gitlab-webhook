f4c8728cc4bfdd3a3f5be9e04d1e3b2bffe68ff8
bigantal <bigantal@gmail.com>
2021-09-28 14:44:58 +0200

    favicon

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
+var favicon = require('serve-favicon')
+var path = require('path')
+
+
 const app = express()
 const port = 3000
+
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public','index.html'));
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
