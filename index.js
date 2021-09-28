dc3c764f3881e39ea368f0879518192a2941a603
Gabor Horvath <gabor.horvath@verint.com>
2021-09-28 21:05:46 +0200

    webhook path comes from url arg

--- index.js
+++ index.js
@@ -1,109 +1,109 @@
-const express = require('express')
-var favicon = require('serve-favicon')
-var path = require('path')
-var bodyParser = require('body-parser')
-const https = require('https')
-
-
+const express = require('express');
+const favicon = require('serve-favicon');
+const path = require('path');
+const bodyParser = require('body-parser');
+const https = require('https');
 
 const app = express()
 const port = process.env.PORT || 3000
 
-
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(bodyParser.json());
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.post('/commit', (req, res) => {
+  console.log('req', req.body)
+});
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'public','index.html'));
-  });
+app.post('/merge-request', (req, res) => {
 
+  const path = req.query.h;
+  console.log('query param [h]:', path);
+  console.log('req', req.body);
 
+  const body = req.body;
+  const oa = body.object_attributes;
 
-  app.post('/merge', (req, res) => {
-      console.log('req',req.body)
-      const body=req.body;
-const oa=body.object_attributes;
-
-     var toTeams= {
-        "@type": "MessageCard",
-        "@context": "http://schema.org/extensions",
-        "themeColor": "0076D7",
-        "summary": "Merge request event",
-        "sections": [
-            {
-                "activityTitle": "Merge request event",
-                "activitySubtitle": oa.title,
-                "activityImage": body.user.avatar_url,
-                "facts": [
-                    {
-                        "name": "Assigned to",
-                        "value": body.assignee.name
-                    },
-                    {
-                        "name": "Status",
-                        "value": oa.action
-                    }
-                ],
-                "markdown": true
-            }
+  const toTeams = {
+    "@type": "MessageCard",
+    "@context": "http://schema.org/extensions",
+    "themeColor": "0076D7",
+    "summary": `Merge Request - ${body.project.name} #${oa.iid}`,
+    "sections": [
+      {
+        "activityTitle": `Merge Request - ${body.project.name} #${oa.iid}`,
+        "activitySubtitle": oa.title,
+        "activityImage": body.project.avatar_url,
+        "facts": [
+          {
+            "name": "Created by",
+            "value": body.user.name
+          },
+          {
+            "name": "Assigned to",
+            "value": body.assignee.name
+          },
+          {
+            "name": "Status",
+            "value": oa.action
+          }
         ],
-        "potentialAction": [
-            {
-                "@type": "OpenUri",
-                "name": "Learn More",
-                "targets": [
-                    {
-                        "os": "default",
-                        "uri": oa.url
-                    }
-                ]
-            }
+        "markdown": true
+      }
+    ],
+    "potentialAction": [
+      {
+        "@type": "OpenUri",
+        "name": "Open",
+        "targets": [
+          {
+            "os": "default",
+            "uri": oa.url
+          }
         ]
-    }
+      }
+    ]
+  };
 
-    sendToTeams(toTeams)
+  sendToTeams(toTeams, path)
 
-
-    res.send(toTeams)
-  })
+  res.send(toTeams)
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`)
 })
 
-function sendToTeams(msg) {
-  
-    const data = JSON.stringify(msg)
-    
-    const options = {
-      hostname: 'verint.webhook.office.com',
-      port: 443,
-      path: '/webhookb2/36683d48-1341-44b4-acd9-484549a79fe0@bb2ed304-4099-49cf-b081-cbb7a3a580ca/IncomingWebhook/b3f9c837c6ca4a23bab624a6cd61b9a3/a4dc01a0-8181-465e-be31-46a1ee71611d',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': data.length
-      }
+function sendToTeams(msg, path) {
+  console.log('sending to MSTeams', msg)
+  const data = JSON.stringify(msg)
+
+  const options = {
+    hostname: 'verint.webhook.office.com',
+    port: 443,
+    path: "/" + path,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length
     }
-    
-    const req = https.request(options, res => {
-      console.log(`statusCode: ${res.statusCode}`)
-    
-      res.on('data', d => {
-        process.stdout.write(d)
-      })
+  }
+
+  const req = https.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`)
+
+    res.on('data', d => {
+      process.stdout.write(d)
     })
-    
-    req.on('error', error => {
-      console.error(error)
-    })
-    
-    req.write(data)
-    req.end()
-    
+  })
+
+  req.on('error', error => {
+    console.error(error)
+  })
+
+  req.write(data)
+  req.end()
 }
